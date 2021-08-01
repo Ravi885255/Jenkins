47bb65a80e1e429c38acd3af998c6ec3a2d93fa5
ravindra <ravindra@Ravindra>
2021-08-01 09:47:08 +0530

    jenkins project

---
+++ src/main/java/Jenkins_DemoProject/Jenkins_DemoProject/App.java
@@ -0,0 +1,13 @@
+package Jenkins_DemoProject.Jenkins_DemoProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/TestCases/TC_01.java
@@ -0,0 +1,24 @@
+package TestCases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_01 {
+	
+	
+	public void browsersetup(String url)
+	{
+		System.setProperty("webdriver.chrome.driver", "Drivers\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(url);
+	}
+	
+	@Test
+	public void Tc_FbLogin()
+	{
+		browsersetup("https://www.fb.com/");
+	}
+
+}
