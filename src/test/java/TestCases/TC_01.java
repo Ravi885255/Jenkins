d54c54b095adc87fa2d21b2eb75ce58acfbf6fe9
ravindra <ravindra@Ravindra>
2021-08-01 12:26:24 +0530

    tets

--- src/test/java/TestCases/TC_01.java
+++ src/test/java/TestCases/TC_01.java
@@ -2,7 +2,6 @@ package TestCases;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
 import org.testng.annotations.Test;
 
 public class TC_01 {
@@ -11,8 +10,7 @@ public class TC_01 {
 	public void browsersetup(String url)
 	{
 		System.setProperty("webdriver.chrome.driver", "Drivers\\chromedriver.exe");
-		//WebDriver driver=new ChromeDriver();
-		WebDriver driver= new ChromeDriver(new ChromeDriverService.Builder().usingPort(34533).build());
+		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get(url);
 	}
